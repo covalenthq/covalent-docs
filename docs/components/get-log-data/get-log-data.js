78530f1064cf863bfb3d4b016509e7fa33077d68
emmaodia <odia.emma@gmail.com>
2021-05-13 16:40:06 +0100

    fix: Get Log By Topic Hash moved to according dropdown

--- docs/components/get-log-data/get-log-data.js
+++ docs/components/get-log-data/get-log-data.js
@@ -2,7 +2,6 @@ import React, { useState, useRef } from "react";
 import "../../css/components/topic-calculator.css";
 import GetContractLogs from "./get-contract-logs";
 import GetTopicHashLogs from "./log-events-topic-hash";
-// import Accordion from "./accordion"
 import "./accordion.css";
 
 const GetLogs = () => {
@@ -23,36 +22,15 @@ const GetLogs = () => {
   return (
     <div>
       <div className="topics">
-      {/* <div>
-     <Accordion
-       title="What is your return policy?"
-       content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-     />
-     
-     <Accordion
-       title="Get Contract Logs"
-       content= {GetContractLogs}
-       />
-       
-       
-   </div> */}
+   <GetContractLogs />
 
    <div className="accordion__section">
      <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
-       <p className="accordion__title">Get Contract Logs</p>
-     </button>
-     <div ref={content} style={{ maxHeight: `${setHeight}` }} className="accordion__content">
-       <GetContractLogs />
-     </div>
-   
-
-   
-     {/* <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
-       <p className="accordion__title">Get Topic Hash Logs</p>
+       <p className="accordion__title">Get Log Events by Topic Hashes</p>
      </button>
      <div ref={content} style={{ maxHeight: `${setHeight}` }} className="accordion__content">
      <GetTopicHashLogs />
-     </div> */}
+     </div>
    </div>
 
        
